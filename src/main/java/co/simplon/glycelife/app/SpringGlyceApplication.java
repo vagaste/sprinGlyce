87fbd955875c736dc8bca335877c97b480e7ae13
unknown <gaelle.thomas.pf1@gmail.com>
2018-06-12 17:04:11 +0200

    my first commit spring

---
+++ src/main/java/co/simplon/glycelife/app/SpringGlyceApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.glycelife.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGlyceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGlyceApplication.class, args);
+	}
+}
---
+++ src/test/java/co/simplon/glycelife/app/SpringGlyceApplicationTests.java
@@ -0,0 +1,16 @@
+package co.simplon.glycelife.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringGlyceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
